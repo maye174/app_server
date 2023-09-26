e1673e33f331c0f0f9a97ccef476f297b6d0caa0
maye174 <96584640+maye174@users.noreply.github.com>
2023-09-26 21:04:11 +0800

    update api

--- src/server/start.cpp
+++ src/server/start.cpp
@@ -6,9 +6,8 @@
 #include "wxpusher/inc/callback.hpp"
 
 void register_callback(struct event_base *base, struct evhttp *http) {
-    evhttp_set_cb(http, "/app/exit/api", api_exit, base);
-    evhttp_set_cb(http, "/app/ewarn/api/creata_qrcode", ewarn_api_create_qrcode,
+    evhttp_set_cb(http, "/api/exit", api_exit, base);
+    evhttp_set_cb(http, "/api/ewarn/creata_qrcode", ewarn_api_create_qrcode,
                   nullptr);
-    evhttp_set_cb(http, "/app/wxpusher_callback/api", wxpusher_callback,
-                  nullptr);
+    evhttp_set_cb(http, "/api/wxpusher_callback", wxpusher_callback, nullptr);
 }
