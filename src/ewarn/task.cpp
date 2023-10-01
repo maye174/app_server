66c5678548c5a7d0e4465b6463ad6c853a8761d4
maye174 <96584640+maye174@users.noreply.github.com>
2023-10-01 17:09:14 +0800

    fix some bug

--- src/ewarn/task.cpp
+++ src/ewarn/task.cpp
@@ -22,33 +22,11 @@ using json = nlohmann::json;
 using list = std::vector<std::tuple<std::string, std::string>>;
 
 // 抓包
-static std::string http_request_done(const std::string &j_s) {
+static std::string http_request_done(const std::string &_suffix) {
 
-    LOG_F(INFO, "%s", j_s.c_str());
+    LOG_F(INFO, "%s", _suffix.c_str());
 
-    json j;
-
-    try {
-        j = json::parse(j_s);
-    } catch (const json::parse_error &e) {
-        LOG_F(ERROR, "解析JSON数据时发生异常 - %s", e.what());
-    } catch (const json::type_error &e) {
-        LOG_F(ERROR, "JSON数据类型错误 - %s", e.what());
-    } catch (const std::exception &e) {
-        LOG_F(ERROR, "发生未知异常 - %s", e.what());
-    }
-
-    if (!j.contains("b") || !j.contains("r")) {
-        LOG_F(ERROR, "未找到building_number 或者 "
-                     "room_number\n 400");
-        return "";
-    }
-
-    std::string building_number = j["b"].get_ref<std::string &>();
-    std::string room_number = j["r"].get_ref<std::string &>();
-
-    std::string suffix = "sysid=1&roomid=" + room_number +
-                         "&areaid=1&buildid=" + building_number;
+    std::string suffix = "sysid=1&areaid=1" + _suffix;
 
     std::string url =
         "http://218.22.140.88:8181/epay/wxpage/wanxiao/eleresult?" + suffix;
