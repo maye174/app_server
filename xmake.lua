2121ee81674fef67ca02bc0b91d13956ba96c39d
maye174 <96584640+maye174@users.noreply.github.com>
2023-10-08 20:21:19 +0800

    fix bug

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,10 @@ add_rules("mode.debug", "mode.release")
 add_requires("libcurl", "libevent", "nlohmann_json", "sqlite3", "openssl", "loguru")
 add_includedirs("src")
 
-includes("src/*/xmake.lua")
+-- includes("src/*/xmake.lua")
 
 target("app_server")
-    add_deps("quickjspp")
+    -- add_deps("quickjspp")
     set_kind("binary")
     add_files("src/*/*.cpp")
     set_languages("cxx17")
